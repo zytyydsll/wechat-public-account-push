d208c437504acf14cd1b29a245f15ad595c9008d
zytyydsll <90997406+zytyydsll@users.noreply.github.com>
2023-01-22 01:00:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,17 +63,17 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '臭宝贝',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oI5Ss6d3Yfx2rzQw-TAR5rLRile4',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'vpRpNkFeEHMn-K1KUGQ203r5Cd943Qe2uMdzyS4CDKo',
       // 所在省份
-      province: '广东',
+      province: '四川',
       // 所在城市
-      city: '肇庆',
+      city: '成都',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-19',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -218,7 +218,7 @@ const USER_CONFIG = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '3aUoP8mqKhVqrWjGfZwgOFLmIXFOrGxwscnvgXre4v8',
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -226,7 +226,7 @@ const USER_CONFIG = {
       // 一般都填自己
       name: '自己',
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oI5Ss6diStDFbii7-onI-CGGby_k',
     }
     // 你可以不断按格式往下增加
     // ...
